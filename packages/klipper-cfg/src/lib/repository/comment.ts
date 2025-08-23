04518b4a94a1112d7a229ee846ae22552e642739
Danny McGee <dannymcgee@gmail.com>
2025-08-23 13:17:08 -0400

    Support semicolon-prefixed line comments (#7)

--- packages/klipper-cfg/src/lib/repository/comment.ts
+++ packages/klipper-cfg/src/lib/repository/comment.ts
@@ -1,10 +1,10 @@
 import { TMGrammarScope } from "@vscode-devkit/grammar";
 
 export const comment: TMGrammarScope = {
-	begin: /#/,
+	name: "comment.line.klipper-cfg",
+	begin: /[#;]/,
 	beginCaptures: {
 		0: { name: "punctuation.definition.begin.comment.line.klipper-cfg" },
 	},
 	end: /(?=[\r\n])/,
-	name: "comment.line.klipper-cfg",
 };
